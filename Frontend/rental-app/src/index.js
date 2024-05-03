aff0fca84ddd053a97f53a8e6684e659947799eb
unknown <aswinshankar1310@gmail.com>
2024-05-03 10:14:48 +0100

    integrated maps

--- Frontend/rental-app/src/index.js
+++ Frontend/rental-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
